b6dfc6ceb446aae316c75f684af286a55c9ef90f
jeffcheema <jeffcheema007@gmail.com>
2016-05-29 17:17:47 -0700

    Add files via upload

---
+++ GMAIL-BOT.py
@@ -0,0 +1,54 @@
+import base64
+import httplib2
+import string
+import random
+from email.mime.text import MIMEText
+
+from apiclient.discovery import build
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.file import Storage
+from oauth2client.tools import run
+
+
+# Path to the client_secret.json file downloaded from the Developer Console
+CLIENT_SECRET_FILE = 'USE YOUR JSON FILE FROM DEVELOPER CONSOLE'
+
+# Check https://developers.google.com/gmail/api/auth/scopes for all available scopes
+OAUTH_SCOPE = 'https://www.googleapis.com/auth/gmail.compose'
+
+# Location of the credentials storage file
+STORAGE = Storage('gmail.storage')
+
+# Start the OAuth flow to retrieve credentials
+flow = flow_from_clientsecrets(CLIENT_SECRET_FILE, scope=OAUTH_SCOPE)
+http = httplib2.Http()
+
+# Try to retrieve credentials from storage or run the flow to generate them
+credentials = None
+if credentials is None or credentials.invalid:
+  credentials = run(flow, STORAGE, http=http)
+
+# Authorize the httplib2.Http object with our credentials
+http = credentials.authorize(http)
+
+# Build the Gmail service from discovery
+gmail_service = build('gmail', 'v1', http=http)
+emailtosend = raw_input("Who would like to send the email to? \n")
+subject = raw_input("What is the subject of your email? \n")
+text = raw_input("What is the message you would like to send? \n")
+number = input("How many times would you like to send this email?")
+warning = raw_input("If you choose a number 500 or above Google may disable your account from \n sending emails for the next 24 hours.(Press enter to continue)")
+# create a message to send
+for _ in " "*number:
+	message = MIMEText(text)
+	message['to'] = emailtosend
+	message['from'] = "you@go.here"
+	message['subject'] = subject + "       " + #''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(3)])
+	body = {'raw': base64.b64encode(message.as_string())}
+	try:
+		message = (gmail_service.users().messages().send(userId="me", body=body).execute())
+		print('Message Id: %s' % message['id'])
+		print(message)
+		print ">>>(((({{{MESSAGE SENT}}))))<<<"
+	except Exception as error:
+		print('An error occurred: %s' % error)
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# gmailSpamBot
+
+This bot sends a email a given number of times.
+
+#>>>BEFORE USING DOWNLOAD PIP<<<
+
+https://pypi.python.org/pypi/pip
+
+#Instructions on How to get .JSON file.
+
+https://console.developers.google.com/start/api?id=gmail
+Use this wizard to create or select a project in the Google Developers Console and automatically enable the API.
+In the sidebar on the left, select APIs & auth and then Consent screen. Select an EMAIL ADDRESS, enter a PRODUCT NAME if not already set, and click the Save button.
+In the sidebar under APIs & auth select Credentials. In the new tab that opens, click Create new Client ID.
+Select the application type Installed application, the installed application type Other, and click the Create Client ID button.
+Click the Download JSON button under your new client ID. Move this file to your working directory and rename it client_secret.json.
+
+#Do not abuse the script.
+
+
+
